615719697f90d3cb2111632eae7748d6b42dc48f
Jamal West <jwest23@ufl.edu>
2022-06-01 13:57:47 -0500

    Time: 128 ms (22.36%), Space: 46 MB (25.40%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -5,29 +5,23 @@
 var evalRPN = function(tokens) {
     let stack = []; // create stack var
     for (let token of tokens) { // read in one element at a time
-        if (token !== '+' && token !== '-' && token !== '*' && token !== '/') {
-            stack.push(Number(token)); // if not an operator push to stack 
-        } else {
-            let op2 = stack.pop(); // store our operands, maintaining left to right for computing sum
-            let op1 = stack.pop();
-            let sum = 0;
-            switch (token) { // based on operand perform computation
-                case ('+'):
-                    sum = op1 + op2;
-                    break;
-                case ('-'):
-                    sum = op1 - op2;
-                    break;
-                case ('*'):
-                    sum = op1 * op2;
-                    break;
-                case ('/'):
-                    sum = op1 / op2;
-                    if (sum >= 0) sum = Math.floor(sum);  // round down if sum pos, else round up if sum neg
-                    else sum = Math.ceil(sum);
-                    break;
+        if (token === "+") {
+            stack.push(stack.pop() + stack.pop()); // pop off two most recent and push their sum
+        } else if (token === "-") {
+            let b = stack.pop(), a = stack.pop(); // maintain order of two most recent pops
+            stack.push(a - b); // push their difference 
+        } else if (token === "*") {
+            stack.push(stack.pop() * stack.pop()); // pop off two most recent and push their product
+        } else if (token === "/") {
+            let b = stack.pop(), a = stack.pop(); // maintain order of two most recent pops
+            if ((a / b) >= 0) {
+                stack.push(Math.floor(a / b)); // push the quotient, rounded down to nearest whole
+            } else {
+                stack.push(Math.ceil(a / b)); // push the quotient, rounded up to nearest whole
             }
-            stack.push(sum); // push sum to stack
+            
+        } else {
+            stack.push(Number(token)); // operator, typecast to int
         }
     }
     return stack[0]; // ret first el in stack as final res
