e6ce27f7d7a841b2edc79e8f779debe7a39e8669
Jamal West <jwest23@ufl.edu>
2022-06-01 21:43:14 -0500

    Time: 414 ms (40.76%), Space: 71.7 MB (17.29%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.js
+++ 739-daily-temperatures/739-daily-temperatures.js
@@ -3,16 +3,16 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temps) {
-    let res = new Array(temps.length).fill(0);
-    let stack = [];
-    for (let i = temps.length - 1; i >= 0; i--) {
-        let temp = temps[i];
-        while (stack.length && stack[stack.length - 1][0] <= temp) {
-            stack.pop();
+    let res = new Array(temps.length).fill(0); // prefill array of length of temps w 0
+    let stack = []; // create stack var
+    for (let i = temps.length - 1; i >= 0; i--) { 
+        let temp = temps[i]; // store curr temp going r -> l
+        while (stack.length && stack[stack.length - 1][0] <= temp) { // if stack !empty & current temp >= stack.top()
+            stack.pop(); // pop until we come across larger temp or until empty
         }
-        let numOfDays = stack.length ? (stack[stack.length - 1][1] - i) : 0;
-        res[i] = numOfDays;
-        stack.push([temp, i]);
+        let numOfDays = stack.length ? (stack[stack.length - 1][1] - i) : 0; // if stack has el, cur idx numOfDays = last hot day - idx
+        res[i] = numOfDays; // store val in res
+        stack.push([temp, i]); // push curr temp and curr idx
     }
-    return res;
+    return res; // return res
 };
