c4aace6bb9f450a738a0008ee00c53cca309a31f
Jamal West <jwest23@ufl.edu>
2022-07-22 18:57:24 -0500

    Time: 150 ms (84.17%), Space: 55.9 MB (97.12%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.js
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.js
@@ -7,24 +7,24 @@ var findDuplicate = function(paths) {
     let res = [], map = new Map();
     for (let p of paths) {
         let i = 0, j, k;
-        while (p.charAt(i) !== ' ') i++;
-        let path = p.slice(0, i); // create root path copy non incl
+        while (p.charAt(i) !== ' ') i++; // incre i idx for next file to be read 
+        let path = p.slice(0, i); // create path var using copy of p up to the file name
         for (j = ++i; i < p.length; i++) { // from start of each file name before increment of i
-            if (p.charAt(i) === '(') k = i; // mark ending of file name
-            else if (p.charAt(i) === ')') {
-                let pathFile = path + '/' + p.slice(j, k); // concatenate pathfile using non incl copy
-                let content = p.slice(k+1, i); // copy content for hashmap
+            if (p.charAt(i) === '(') k = i; // mark ending of file name using k idx
+            else if (p.charAt(i) === ')') { // i idx has read in file content
+                let pathFile = path + '/' + p.slice(j, k); // create pathfile using base path and p.slice from j to k 
+                let content = p.slice(k+1, i); // create content for hashmap using copy from k+1 to i 
                 if (!map.has(content)) {
                     map.set(content, [pathFile]);
                 } else {
                     map.get(content).push(pathFile);
                 }
-                j = i + 2; // move to next file in path
+                j = i + 2; // move j idx to next file in path using i
             }
         }
     }
-    for (let v of map.values()) {
-        if (v.length > 1) res.push(v);
+    for (let v of map.values()) { // iterate hashmap values, checking for dupl
+        if (v.length > 1) res.push(v); // if found, push to res
     }
     return res;
     
