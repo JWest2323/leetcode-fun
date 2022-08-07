a4c157c04536eca03758a3fe5b9b788edbfd517b
Jamal West <jwest23@ufl.edu>
2022-08-07 16:05:11 -0500

    Time: 112 ms (36.31%), Space: 45.5 MB (29.72%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -12,5 +12,17 @@
  */
 var maxDepth = function(root) {
     if (!root) return 0;
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    let depth = 1;
+    let q = [[root, depth]]; // init queue w root node and starting depth
+    while (q.length > 0) {
+        let pop = q.pop();
+        if (pop[0] !== null) { // check for valid node
+            if (pop[1] > depth) depth = pop[1]; // update depth if nec
+        }
+        let newDepth = pop[1] + 1; // incr depth for child nodes if any
+        if (pop[0].left) q.push([pop[0].left, newDepth]);
+        if (pop[0].right) q.push([pop[0].right, newDepth]);
+    }
+    return depth; // return depth
+    
 };
