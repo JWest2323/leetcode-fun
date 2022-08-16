9a85f8863866abfdd7f5ab935de39a78fc2ce076
Jamal West <jwest23@ufl.edu>
2022-08-16 18:42:44 -0500

    Time: 116 ms (79.12%), Space: 46.7 MB (80.13%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,20 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-// O(n) time complexity using sliding window technique
 var lengthOfLongestSubstring = function(s) {
-    if (!s) return 0; // input handler
-    let struct = new Set(); // create a set to check for duplicates
-    let l = 0; // create left ptr for sliding window
-    let max = 0; // create max ret var
-   
+    if (!s) return 0;
+    let set = new Set(); // create set to store chars
+    let l = 0, max = 0; // create l ptr for sliding win & max count var to track largest window
     for (let r = 0; r < s.length; r++) {
-        while (struct.has(s[r])) { // while s[r] is in set
-            struct.delete(s[l]); // remove from the left until dupl removed
-            l++; // increment l to update window
+        while (set.has(s[r])) { // if dupl found
+            set.delete(s[l]); // delete from set 
+            l++; // shrink from left
         }
-        struct.add(s[r]); // no dupl in set, add cur char
-        max = Math.max(max, r - l + 1); // recalculate max
+        set.add(s[r]); // no dupl in set, add s[r]
+        max = Math.max(max, set.size); // recalc max using length of set
     }
-    return max; // return max once for loop complete
+    return max;
 };
