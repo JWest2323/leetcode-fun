68bd67bf582076bd03dbd9ff37019fd07067c164
Jamal West <jwest23@ufl.edu>
2022-08-18 16:48:21 -0500

    Time: 68 ms (84.94%), Space: 41.5 MB (98.19%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -2,27 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-// O(n * log(nm)) t.c. 
-// log(nm) binary search and n loops of nums arr
-
+// O(n) t.c. 
 var minStartValue = function(nums) {
-    let n = nums.length, m = 100; // store nums.length & init safe max int val
-    let l = 1, r = m * n + 1; // init l & r ptrs using m * n + 1 as upper bound
+    let [sum, min] = [0, nums[0]]; // init sum to 0 & min to 1st el in nums
     
-    while (l < r) {
-        let mid = Math.floor(l + (r - l) / 2); // calc a midpoint to test as startVal
-        let startVal = mid;
-        let isValid = true;
-        for (let num of nums) {
-            startVal += num;
-            if (startVal < 1) { // check if startVal went neg. 
-                isValid = false;
-                break; // stop for loop
-            }
-        }
-        if (isValid) r = mid; // update r ptr to check for smaller startVal
-        else l = mid + 1; // update l to check for larger startVal
+    for (let num of nums) {
+        sum += num; // add each num in nums arr to sum
+        min = Math.min(min, sum); // update our min var each time
     }
-    return l; // return l or minStartVal
+    return min > 0 ? 1 : 1 - min; // if min never went neg, ret 1 else 1 - min
 };
