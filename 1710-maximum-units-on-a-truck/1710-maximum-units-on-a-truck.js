0a0bf1d07dafd41e60141b3c92a69fa6407d20b7
Jamal West <jwest23@ufl.edu>
2022-08-20 12:40:34 -0500

    Time: 112 ms (58.65%), Space: 45.5 MB (19.73%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -4,14 +4,25 @@
  * @return {number}
  */
 var maximumUnits = function(boxTypes, truckSize) {
-    if (!boxTypes || truckSize === 0) return 0;
-    let unitCount = 0; // create return var
-    boxTypes.sort((a, b) => b[1] - a[1]); // sort descend. based on numOfUnitsPerBox
-    for (let boxType of boxTypes) {
-        let boxCount = Math.min(boxType[0], truckSize); // set boxCount to min btwn curr numOfBoxes & remaining truckSize  
-        unitCount += boxCount * boxType[1]; // incr. unitCount by total boxes
-        truckSize -= boxCount; // decre. truckSize
-        if (truckSize === 0) break;
+    let bucket = []; // init bucket arr to perform bucket sort
+    let total = 0; // init ret var
+    // loop the els of boxTypes
+    for (let i = 0; i < boxTypes.length; i++) {
+        let [numBox, numUnit] = boxTypes[i];
+        if (!bucket[numUnit]) bucket[numUnit] = numBox; // if new boxType, set numOfBoxes
+        else bucket[numUnit] += numBox;                 // else incre. numOfUnits
     }
-    return unitCount;
+    // loop starting from end of bucket arr to target boxTypes with higher numUnit
+    for (let numUnit = bucket.length - 1; numUnit >= 0; numUnit--) {
+        if (!bucket[numUnit]) continue;
+        let numBox = bucket[numUnit];                   // store num of boxes at curr numUnit
+        if (truckSize >= numBox) {                      // check against truckSize
+            total += (numBox * numUnit);
+            truckSize -= numBox;
+        } else {                                        // fill remaining space in truck & return
+            total += (truckSize * numUnit);
+            return total;
+        }
+    }
+    return total;
 };
