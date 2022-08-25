b927ea3e2fba46654e35c559580d576e8e8ee2f7
Jamal West <jwest23@ufl.edu>
2022-08-25 14:37:22 -0500

    Time: 192 ms (20.17%), Space: 50.2 MB (36.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,41 +3,40 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    if (!grid) return 0; // if null grid ret 0
-    const WATER = '0', LAND = '1'; // predefine water & land
-    const DIR = [[1, 0], [0, 1], [0, -1], [-1, 0]]; // up row, up col, down row, down col
+    let dirs = [[-1,0], [1,0], [0,-1], [0,1]];
+    const WATER = '0', LAND = '1';
     
-    function bfs (grid, r, c) {
+    function bfs(grid, r, c) {
         let q = [[r, c]];
         grid[r][c] = WATER;
         while (q.length > 0) {
-            let numOfNbrs = q.length;
-            for (let i = 0; i < numOfNbrs; i++) {
+            let size = q.length;
+            for (let i = 0; i < size; i++) {
                 let [row, col] = q.pop();
                 
-                for (let [x, y] of DIR) {
-                    let nRow = row + x;
-                    let nCol = col + y;
-                    if (nRow < 0 || nRow >= grid.length   || 
-                        nCol < 0 || nCol > grid[0].length || 
-                        grid[nRow][nCol] !== LAND) { // if new direction = OOB or !LAND: continue through DIRs
+                for (let [x, y] of dirs) {
+                    let newRow = row + x;
+                    let newCol = col + y;
+                    
+                    if (newRow < 0 || newRow >= grid.length || newCol < 0 ||
+                        newCol >= grid[0].length || grid[newRow][newCol] !== LAND) {
                         continue;
                     }
-                    grid[nRow][nCol] = WATER; // set to water so we dont revisit
-                    q.unshift([nRow, nCol]); // add to front of q to bfs next
+                    grid[newRow][newCol] = WATER; 
+                    q.unshift([newRow, newCol]);
                 }
             }
         }
     }
     
-    let numOfIslands = 0; // create ret var
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === LAND) { // if we come across LAND    
-                numOfIslands++; // increment counter
-                bfs(grid, i, j); // check all neighbors
+    let islands = 0;
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (grid[r][c] === LAND) {
+                islands++;
+                bfs(grid, r, c);
             }
         }
     }
-    return numOfIslands;
+    return islands;
 };
