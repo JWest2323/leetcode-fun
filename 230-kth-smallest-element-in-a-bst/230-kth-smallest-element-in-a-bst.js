17701268ddf5987fb980cce448e35d4d8aaafec5
Jamal West <jwest23@ufl.edu>
2022-10-03 21:55:46 -0500

    Time: 115 ms (58.06%), Space: 48.2 MB (63.70%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let stack = [];
+    let count = 1, node = root;
+    while (node || stack.length) {
+        while (node) {
+            stack.push(node);
+            node = node.left;
+        }
+        node = stack.pop();
+        if (count == k) return node.val;
+        else count++;
+        node = node.right;
+    }
+};
