07db0d30102b3530505ff8c65ac9274c6e03470d
Jamal West <jwest23@ufl.edu>
2023-05-24 14:16:01 -0500

    Time: 74 ms (83.88%), Space: 51.1 MB (93.02%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -14,17 +14,18 @@ var maxPathSum = function(root) {
     let maxPath = root.val;
     
     const dfs = (node) => {
-        if (!node) return 0;
+        if (!node) return 0; // return 0 for leaf nodes
         
+        // use induction to compute the subtree's max sum 
         let maxLeftSum = dfs(node.left);
         let maxRightSum = dfs(node.right);
 
-        maxPath = Math.max(maxPath, node.val + maxLeftSum + maxRightSum);
+        maxPath = Math.max(maxPath, node.val + maxLeftSum + maxRightSum); // update max against current path
         
         return Math.max(node.val + maxLeftSum, node.val + maxRightSum, 0) // include 0 to trim neg. paths
     }
     
-    dfs(root);
+    dfs(root); // call on root
     
-    return maxPath;
+    return maxPath; // return res var
 };
