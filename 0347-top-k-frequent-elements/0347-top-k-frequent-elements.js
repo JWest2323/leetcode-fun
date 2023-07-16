01f652e5e9f0b243fc97bd32a1838d0af4931fb9
Jamal West <jwest23@ufl.edu>
2023-07-15 21:32:52 -0500

    Time: 71 ms (80.14%), Space: 44.9 MB (70.67%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -4,25 +4,24 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let map = new Map();
+    let res = [], q = [], map = new Map();
     
+    // count the frequency of values
     for (let num of nums) {
-        // for each new element found, init val to 1, else incre by 1
-        !map.has(num) ? map.set(num, 1) : map.set(num, map.get(num) + 1);
+        if (!map.has(num)) {
+            map.set(num, 1);
+        } else {
+            map.set(num, map.get(num) + 1);
+        }
     }
     
-    // sort [key,val] of map by largest freq
-    let sortedEntries = [...map.entries()].sort((a, b) => b[1] - a[1]);
+    q = [...map.entries()]; // store entries of map in q
+    q.sort((a, b) => b[1] - a[1]); // sort q in decending order
     
-    //init res arr for return
-    let res = [];
-    
-    // shift from sortedEntries k times
-    while (k > 0) {
-        res.push(sortedEntries.shift()[0]); // push k most freq num to res arr
-        k--;
+    // shift off q k times and push to res
+    for (let i = 0; i < k; i++) {
+        res.push(q.shift()[0]);
     }
-    
+    // return res
     return res;
-    
 };
