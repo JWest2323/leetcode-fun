e6849d34f5b5ac3b328054ced7fd152bf1b0cf37
Jamal West <thejamalwest@gmail.com>
2024-04-06 13:06:59 -0500

    Time: 66 ms (62.87%), Space: 57.7 MB (17.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,27 +3,17 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    // two pointer approach starting at beginning and end of height arr
-    let max = 0, l = 0, r = height.length - 1;
-
-    // calc max area while ptrs havent crossed
+    let res = 0, l = 0, r = height.length - 1;
+    
     while (l < r) {
-        let len = Math.min(height[l], height[r]); // use smaller of two lens
-        let width = r - l; // use difference in idx to calc width
+        let curArea = (Math.min(height[l], height[r])) * (r - l);
+        res = Math.max(res, curArea);
         
-        // update max area
-        max = Math.max(max, len * width)
-
-        // pointer policy: move ptr w smaller height, if === move both
-        if (height[l] < height[r]) { 
+        if (height[l] < height[r]) {
             l++;
-        }
-        else if (height[l] > height[r]) {
-            r--;
         } else {
-            l++, r--;
+            r--;
         }
     }
-
-    return max;
+    return res;
 };
