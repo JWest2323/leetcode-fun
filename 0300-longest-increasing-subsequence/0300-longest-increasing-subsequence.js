ccd3673623361812d3939b54c6b2b5ed040048dd
Jamal West <thejamalwest@gmail.com>
2024-04-10 15:07:32 -0500

    Time: 141 ms (52.19%), Space: 50.5 MB (67.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -3,19 +3,11 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
-    // init dp arr of size nums.length and fill w 1 for smallest incre subseq
     let dp = new Array(nums.length).fill(1);
     
-    // loop from 1 to length of nums
-    for (let i = 1; i < nums.length; i++) {
-        // loop from 0 to i 
-        for (j = 0; j < i; j++) {
-            if (nums[i] > nums[j]) { // if an increasing subseq found
-                // update the spot in index of dp using math.max
-                dp[i] = Math.max(dp[i], dp[j] + 1); 
-            }
-        }
-    }
-    // return largest value found in dp
+    for (let i = 1; i < nums.length; i++)
+        for (let j = 0; j < i; j++)
+            if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], 1 + dp[j]);
+    
     return Math.max(...dp);
 };
