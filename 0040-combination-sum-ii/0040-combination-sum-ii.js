321d848c38dddc1fb8b8ad0d52f9f91e5f523696
Jamal West <thejamalwest@gmail.com>
2024-04-10 21:15:12 -0500

    Time: 57 ms (94.57%), Space: 52 MB (90.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -4,32 +4,25 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    // sort candidates arr to be used for backtracking and to avoid duplicates
     candidates.sort((a, b) => a - b);
-    
     let res = [];
     
-    const backtrack = (idx, remaining, combo) => {
-        // remaining === 0 implies current combo === target
-        if (remaining === 0) { 
-            res.push(combo);
+    const backtrack = (cur, pos, target) => {
+        if (target == 0)
+            res.push(cur.slice());
+        if (target <= 0)
             return;
-        }
         
-        // loop from current index to find other candidates to reach target
-        for (let i = idx; i < candidates.length; i++) {
-            // if candidate at this index <= remaining, we can add
-            if (candidates[i] <= remaining) {
-                backtrack(i + 1, remaining - candidates[i], [...combo, candidates[i]]);
-            }
-            // continue iterator for duplicate values
-            while (candidates[i + 1] === candidates[i]) i++;
+        let prev = -1;
+        for (let i = pos; i < candidates.length; i++) {
+            if (candidates[i] == prev) continue;
+            cur.push(candidates[i]);
+            backtrack(cur, i + 1, target - candidates[i]);
+            cur.pop();
+            prev = candidates[i];
         }
-        
-        // return res arr
-        return res;
     }
+    backtrack([], 0, target);
+    return res;
     
-    // return backtrack function from 0 index, target = remaining, & empty combo arr
-    return backtrack(0, target, []);
 };
