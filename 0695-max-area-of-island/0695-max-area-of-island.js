045eed4bc2225c780a336801bbb10774e6d691fc
Jamal West <thejamalwest@gmail.com>
2024-05-15 15:26:19 -0500

    Time: 69 ms (66.61%), Space: 52.8 MB (48.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    let m = grid.length, n = grid[0].length;
-    let max = 0;
-    
-    function dfs (i, j) {
-        // if OOB ret 0
-        if (i < 0 || j < 0 || i >= m || j >= n || !grid[i][j]) return 0; 
-        // implicitly mark spot
-        grid[i][j] = 0;
-        // ret 1 + dfs on 4 dirs
-        return (1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1));
+    let res = 0, m = grid.length, n = grid[0].length;
+
+    const dfs = (row, col) => {
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == '*' || grid[row][col] == 0)
+            return 0;
+        
+        grid[row][col] = '*';
+        
+        return (1 + dfs(row + 1, col) +
+                    dfs(row - 1, col) +
+                    dfs(row, col + 1) +
+                    dfs(row, col - 1));
     }
-    // traverse grid using dbl for
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j]) // if spot in grid == 1
-                max = Math.max(max, dfs(i, j)); // update max by calling dfs
+
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+            res = Math.max(dfs(r, c), res);
         }
     }
-    
-    return max;
+    return res;
 };
