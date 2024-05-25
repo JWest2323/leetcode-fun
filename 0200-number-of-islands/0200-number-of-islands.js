b40bde84d34488297a0a1ca74b666c3ee815d9d3
Jamal West <thejamalwest@gmail.com>
2024-05-25 16:53:13 -0500

    Time: 157 ms (6.05%), Space: 63.9 MB (8.60%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,27 +3,30 @@
  * @return {number}
  */
 var numIslands = function(grid) {
+    let res = 0, vis = new Set();
     let m = grid.length, n = grid[0].length;
-    let islands = 0;
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
     
-    function dfs (r, c) {
-        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] !== '1')
-            return;
-        grid[r][c] = '0';
-        
-        dfs(r + 1, c);
-        dfs(r - 1, c);
-        dfs(r, c + 1);
-        dfs(r, c - 1);
+    const dfs = (row, col) => {
+        if (row < 0 || col < 0 || row >= m || col >= n ||
+            vis.has(`${row}-${col}`) || grid[row][col] == 0)
+                return;
+        vis.add(`${row}-${col}`);
+        for (let [x, y] of dirs) {
+            let xDir = row + x, yDir = col + y;
+            if (!vis.has(xDir-yDir))
+                dfs(xDir, yDir);
+        }
     }
     
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
-            if (grid[r][c] == '1')
-                islands++;
+            if (grid[r][c] == 1 && !vis.has(`${r}-${c}`)) {
                 dfs(r, c);
+                res++;
+            }
         }
     }
     
-    return islands;
+    return res;
 };
