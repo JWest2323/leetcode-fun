8078d6c636baf979091f0f3b929f4bf404c81efa
Jamal West <thejamalwest@gmail.com>
2024-06-03 20:19:14 -0500

    Time: 73 ms (28.96%), Space: 57.6 MB (14.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let res = 0, l = 0, r = height.length - 1;
-    
+    let res = -Infinity, l = 0, r = height.length - 1;
+
     while (l < r) {
-        let curArea = (Math.min(height[l], height[r])) * (r - l);
+        let curArea = (Math.min(height[l], height[r]) * (r - l));
         res = Math.max(res, curArea);
-        
-        if (height[l] < height[r]) {
+
+        if (height[l] <= height[r]) {
             l++;
         } else {
             r--;
