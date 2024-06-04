2e9f9de4acf1301ff514a988369aa4c8655902db
Jamal West <thejamalwest@gmail.com>
2024-06-04 17:57:59 -0500

    Time: 61 ms (98.59%), Space: 54.5 MB (68.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,13 +12,24 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    if (!preorder.length || !inorder.length) 
-        return null;
+    let preorderIdx = 0;
+    let inorderIdxMap = new Map();
     
-    let root = new TreeNode(preorder[0]);
-    let mid = inorder.indexOf(preorder[0]);
-    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
-    root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+    for (let i = 0; i < inorder.length; i++) {
+        inorderIdxMap.set(inorder[i], i);
+    }
     
-    return root;
+    const arrToTree = (left, right) => {
+        if (left > right) return null;
+        
+        let rootVal = preorder[preorderIdx++];
+        let root = new TreeNode(rootVal);
+        
+        root.left = arrToTree(left, inorderIdxMap.get(rootVal) - 1);
+        root.right = arrToTree(inorderIdxMap.get(rootVal) + 1, right);
+        
+        return root;
+    }
+    
+    return arrToTree(0, inorder.length - 1);
 };
