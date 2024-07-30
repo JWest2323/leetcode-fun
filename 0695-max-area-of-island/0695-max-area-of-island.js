642df4c8310acd7088416e03e375f84724ece688
Jamal West <thejamalwest@gmail.com>
2024-07-29 19:33:20 -0500

    Time: 70 ms (66.70%), Space: 52 MB (61.61%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -2,29 +2,29 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var maxAreaOfIsland = function (grid) {
-    let res = -Infinity, vis = new Set();
+var maxAreaOfIsland = function(grid) {
+    let res = 0;
     let m = grid.length, n = grid[0].length;
 
     const dfs = (row, col, curArea) => {
-        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] !== 1
-            || vis.has(`${row}-${col}`))
+        if (row < 0 || col < 0 || row >= m || col >= n || grid[row][col] != 1)
             return 0;
 
-        vis.add(`${row}-${col}`);
+        grid[row][col] = "*";
 
-        curArea += 1 + (dfs(row + 1, col, curArea) + dfs(row - 1, col, curArea)
-            + dfs(row, col + 1, curArea) + dfs(row, col - 1, curArea));
-
+        curArea += 1 + dfs(row + 1, col, curArea) + dfs(row - 1, col, curArea) 
+                    + dfs(row, col + 1, curArea) + dfs(row, col - 1, curArea);
+        
         return curArea;
     }
 
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
-            if (grid[r][c] === 1 && !vis.has(`${r}-${c}`))
-                res = Math.max(res, dfs(r, c, 0));
+            if (grid[r][c] == 1) {
+                let curArea = dfs(r, c, 0);
+                res = Math.max(res, curArea);
+            }
         }
     }
-
-    return res === -Infinity ? 0 : res;
+    return res;
 };
