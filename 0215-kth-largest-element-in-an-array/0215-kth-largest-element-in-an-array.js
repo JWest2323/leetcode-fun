1dd60f592806d56cc431b6625d8aea7b264e12ae
Jamal West <thejamalwest@gmail.com>
2024-09-16 20:11:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,12 +4,35 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    let pq = new MinPriorityQueue();
-    for (let num of nums) {
-        pq.enqueue(num);
-        if (pq.size() > k) {
-            pq.dequeue();
+    const pivotHelper = (pivot, start, end) => {
+         // move the pivot to the end (get it out of the way)
+        swap(pivot, end);
+        let i = start, j = start;
+
+        // move smaller nums to the begining of the array
+        while (j < end) {
+            if (nums[j] <= nums[end])
+                swap(i, j), i++;
+            j++;
         }
+        // swap pivot to its final position
+        swap(i, end);
+        return i;
     }
-    return pq.front().element;
+    const swap = (i, j) => {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    let finalIdx = nums.length - k;
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let pivot = Math.floor(Math.random() * (right - left + 1)) + left;
+        let pivotIdx = pivotHelper(pivot, left, right);
+
+        if (pivotIdx == finalIdx) return nums[finalIdx];
+
+        if (pivotIdx < finalIdx) left = pivotIdx + 1;
+        else right = pivotIdx - 1;
+    }
 };
