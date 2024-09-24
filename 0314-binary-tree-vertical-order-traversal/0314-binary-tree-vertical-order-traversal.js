02a9797de96aa735cf95c5408c1e01d89191bd3c
Jamal West <thejamalwest@gmail.com>
2024-09-24 14:25:38 -0500

    Time: 62 ms (54.3%), Space: 51.6 MB (57.43%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.js
@@ -11,26 +11,31 @@
  * @return {number[][]}
  */
 var verticalOrder = function(root) {
-    if (!root) return [];
-    
-    let map = {}, q = [[root, 0]];
-    let min = 0, max = 0;
+    let nodes = new Map();
 
-    while (q.length) {
-        let [node, col] = q.shift();
+    const dfs = (node, depth, col) => {
+        if (!node) return;
+        node.depth = depth;
+        
+        if (!nodes.has(col)) nodes.set(col, []);
+        nodes.get(col).push(node);
 
-        map[col] = [...(map[col] ?? []), node.val];
-        min = Math.min(min, col);
-        max = Math.max(max, col);
-
-        if (node.left) q.push([node.left, col - 1]);
-        if (node.right) q.push([node.right, col + 1]);
+        dfs(node.left, depth + 1, col - 1);
+        dfs(node.right, depth + 1, col + 1);
     }
 
-    let res = [];
+    dfs(root, 0, 0);
 
-    while (min <= max) 
-        res.push(map[min++]);
+    for (let cols of nodes.values()) 
+        cols.sort((a, b) => a.depth - b.depth);
     
+    let res = [];
+    let minCol = Math.min(...nodes.keys());
+    let maxCol = Math.max(...nodes.keys());
+
+    for (let colIdx = minCol; colIdx <= maxCol; colIdx++) {
+        let columns = nodes.get(colIdx);
+        res.push(columns.map(el => el.val));
+    }
     return res;
 };
