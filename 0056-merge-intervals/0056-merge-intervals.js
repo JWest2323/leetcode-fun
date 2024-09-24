014f8fbc27b043d88600da87d566466bfe78be76
Jamal West <thejamalwest@gmail.com>
2024-09-24 17:06:58 -0500

    Time: 99 ms (45.17%), Space: 59.6 MB (38.3%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -3,26 +3,14 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (intervals.length == 1) return intervals;
-    
-    // sort intervals array by start
     intervals.sort((a, b) => a[0] - b[0]);
-    
-    // init res array w first sorted interval
-    let res = [intervals[0]];
-    
-    // loop from idx = 1
-    for (let [start, end] of intervals.slice(1)) {
-        
-        // if curr start > prev end: push cur start to res
-        if (start > res[res.length - 1][1]) {
-            res.push([start, end]);
-            
-        } else {
-            
-            // update prev end using cur end
-            res[res.length - 1][1] = Math.max(end, res[res.length - 1][1]);
-        }
+    let merged = [];
+
+    for (let interval of intervals) {
+        if (!merged.length || merged[merged.length - 1][1] < interval[0]) 
+            merged.push(interval);
+        else 
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1]);
     }
-    return res;
+    return merged;
 };
