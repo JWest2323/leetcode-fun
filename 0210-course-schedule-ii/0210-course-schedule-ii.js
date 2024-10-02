b970dbdb91da3722a8c6c85d2576aa2b161534cf
Jamal West <thejamalwest@gmail.com>
2024-10-02 13:15:09 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -4,32 +4,29 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-    let adjList = new Map(), indegree = new Array(numCourses).fill(0);
-    let res = [];
+    let indegree = new Array(numCourses).fill(0);
+    let reqs = new Map(), res = [];
 
-    for (let [course, prereq] of prerequisites) {
-        if (!adjList.has(prereq)) 
-            adjList.set(prereq, []);
-        adjList.get(prereq).push(course);
+    for (let [course, need] of prerequisites) {
         indegree[course]++;
-    }   
+        if (!reqs.has(need)) reqs.set(need, []);
+        reqs.get(need).push(course);
+    }
 
-    let q = indegree.map((idx) => idx).filter((idx) => indegree[idx] == 0);
-    let i = 0;
+    let q = [];
+    for (let i = 0; i < indegree.length; i++) 
+        if (!indegree[i]) 
+            q.push(i);
+
     while (q.length) {
-        let course = q.shift();
-        res.push(course);
-        i++;
-
-        if (adjList.has(course)) {
-            for (let nbr of adjList.get(course)) {
-                indegree[nbr]--;
-                if (indegree[nbr] == 0) q.push(nbr);
-            }
+        let curr = q.shift();
+        res.push(curr);
+        let nbrs = reqs.get(curr) || [];
+        console.log(nbrs)
+        for (let nbr of nbrs) {
+            indegree[nbr]--;
+            if (!indegree[nbr]) q.push(nbr);
         }
     }
-    if (i == numCourses) 
-        return res;
-    else 
-        return [];
+    return res;
 };
